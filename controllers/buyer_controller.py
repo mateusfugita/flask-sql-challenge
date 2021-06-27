44d84139993c98801953cdae9f408b462a219820
Mateus Fugita <mateushiwao@gmail.com>
2021-06-27 00:26:19 -0300

    fix error from nullable values

--- controllers/buyer_controller.py
+++ controllers/buyer_controller.py
@@ -9,6 +9,9 @@ AddressState = state.AddressState
 
 def create(data):
     try:
+        if(data['address_city_id'] != None and checkIfAddressCityExists(data['address_city_id']) == False):
+            return { 'status': 400, 'message': 'ID da cidade não existe' }
+        
         buyer = buy.Buyer(first_name=data['first_name'], 
                 last_name=data['last_name'],
                 document=data['document'], 
@@ -29,15 +32,20 @@ def selectOne(id: int):
                 .join(AddressCity, Buyer.address_city_id == AddressCity.id).join(AddressState, AddressCity.address_state_id == AddressState.id).filter(Buyer.id == id).first()
 
         if result == None:
-            return { 'status': 200, 'message': 'Nenhum resultado encontrado' }
+            return { 'status': 404, 'message': 'Nenhum resultado encontrado' }
         
         response = {
             'status': 200,
-            'full_name': result[0] + ' ' + result[1],
+            'full_name': (result[0] or '') + ' ' + (result[1] or ''),
             'email': result[2],
-            'address': result[3] + ' - ' + result[4] + '/' + result[5]
+            'address': (result[3] or '') + ' - ' + (result[4] or '') + '/' + (result[5] or '')
         }
         return response
     except Exception as e:
         print(e)
         return { 'status': 400, 'message': 'Erro na requisição' }
+
+def checkIfAddressCityExists(id):
+    address_city = AddressCity.query.filter_by(id = id).first()
+    return address_city is not None
+        
